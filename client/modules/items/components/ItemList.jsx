141953765885ab7696f62741908d893d8e1e083f
David A. Ortega <dortega@iRuinYaAir.local>
2016-02-27 18:42:10 -0800

    Chapter 4

--- client/modules/items/components/ItemList.jsx
+++ client/modules/items/components/ItemList.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Item from './Item.jsx';
+import { Row, Col } from 'react-bootstrap';
 
 const ItemList = ({content = () => null }) => (
-  <div>
-    <p>This is where the items will be.</p>
-  </div>
+  <Row className="show-grid">
+    <Col xs={12}>
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />      
+    </Col>
+  </Row>
 );
 
 export default ItemList;
